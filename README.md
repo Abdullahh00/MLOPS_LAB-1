b4af83e45e390e2d4b884874049fb9071f11e718
faizan2ahmed <139555971+faizan2ahmed@users.noreply.github.com>
2024-02-13 11:15:50 +0500

    >> Modified Main.py >> Created makefile >> Modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MLOPS_LAB-1
+
+>> Dev branch created by i200933
+>> Test branch created by i200933
+>> i200933 branch created by i200933
+>> Main.py file created by i200933 and merged into dev branch
+>> i200546 branch created by i200546
+>> main.py modified by i200546
+>> makefile created by i200546 
+>> README.md file modified by i200546
--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def subtract(x, y):
     """Function to subtract two numbers."""
     return x - y
 
+def multiply(x, y):
+    """Function to multiply two numbers."""
+    return x * y
+
+def divide(x, y):
+    """Function to divide two numbers."""
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
 
 
 def main():
@@ -15,6 +25,8 @@ def main():
 
     print("1. Addition")
     print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
     
 
     choice = input("Enter choice (1/2/3/4): ")
@@ -23,6 +35,10 @@ def main():
         print("Result:", add(num1, num2))
     elif choice == '2':
         print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
     else:
         print("Invalid input")
 
